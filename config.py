d03fa00ddd5e39969535db2f2f20a158afb3365f
daniyar.kalmurzin <daniyar.kalmurzin@gmail.com>
2025-03-10 16:37:38 +0500

    fix config

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 TELEGRAM_BOT_TOKEN= os.getenv('TELEGRAM_BOT_TOKEN', None)
 USER_SPECIFIC_ID = int(os.getenv('USER_SPECIFIC_ID', -1))
 MINI_PRINTER = os.getenv('MINI_PRINTER', 'Canon_LBP3010_LBP3018_LBP3050')
-USERS = os.getenv('USER_SPECIFIC_ID=145091384', '')
+USERS = os.getenv('USERS', '')
 
 def auth_user():
     return [int(user) for user in USERS.split(',') if user]
